7c8782aa1e31a61db74fba18a64fe39fcb6e9539
Petar Benov <59176961+PetarBenov@users.noreply.github.com>
2020-04-22 16:15:50 +0300

    Add files via upload

---
+++ Brains/js/app.js
@@ -0,0 +1,18 @@
+	$(window).scroll(function(e) {
+
+    // add/remove class to navbar when scrolling to hide/show
+    var scroll = $(window).scrollTop();
+    if (scroll >= 150) {
+        $('.navbar').addClass("navbar-hide");
+    } else {
+        $('.navbar').removeClass("navbar-hide");
+    }
+
+    lightbox.option({
+      	'resizeDuration': 200,
+      	'wrapAround': true,
+      	'alwaysShowNavOnTouchDevices': true
+    });
+
+
+});
